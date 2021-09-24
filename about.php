a7905e53470aa24560a5d42d07b65b6a1b626937
Sergio Cortez <sercor_12@outlook.es>
2021-09-23 23:37:08 -0500

    add about page

---
+++ about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre nosotros</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+    <!-- Simple line icons-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css" rel="stylesheet" />
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="assets/css/styles2.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Navigation-->
+    <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
+    <nav id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand text-white font-weight-bold">Sistema de quejas</li>
+            <li class="sidebar-nav-item"><a href="./">Inicio</a></li>
+            <li class="sidebar-nav-item"><a href="./auth/userLogin.php">Iniciar sesion</a></li>
+            <li class="sidebar-nav-item"><a href="./auth/crearUsuario.php">Crear nueva cuenta</a></li>
+            <li class="sidebar-nav-item"><a href="about.php">Acerca de nosotros</a></li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <h2 class="h2 mt-5">Acerda de nosotros</h2>
+
+        <p>Somos un sistema de reporte de quejas que permite dar seguimiento a las quejas, mostrar estadísticas y llevar un control de los usuarios registrados</p>
+    </div>
+
+    <script src="assets/js/scripts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
