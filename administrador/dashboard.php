91a454a05de73f732dc8ab73d99d20cefea9409c
Sergio Cortez <sercor_12@outlook.es>
2021-09-30 20:52:25 -0500

    validate admin user of pages

--- administrador/dashboard.php
+++ administrador/dashboard.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['id_usuario']) || $_SESSION['tipo_usuario'] != 1) {
+    header("Location: ../auth/adminLogin.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
