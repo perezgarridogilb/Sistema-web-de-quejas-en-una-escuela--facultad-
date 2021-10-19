30df2292ec3b5b8713b38c241459adc621a43508
Sergio Cortez <sercor_12@outlook.es>
2021-10-19 12:08:41 -0500

    only show admin page for users type 1

--- layout/menu.php
+++ layout/menu.php
@@ -3,6 +3,7 @@ $rootProjectPath = "/Otono2021/Sistema-web-de-quejas-en-una-escuela-facultad";
 ?>
 
 <!-- Navigation-->
+
 <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
 <nav id="sidebar-wrapper">
     <ul class="sidebar-nav">
@@ -22,7 +23,7 @@ $rootProjectPath = "/Otono2021/Sistema-web-de-quejas-en-una-escuela-facultad";
             echo "<li class='sidebar-nav-item'><a href='$rootProjectPath/dashboard.php'>Estadísticas</a></li>";
         }
         echo "<li class='sidebar-nav-item'><a href='$rootProjectPath/reports/listReports.php'>Listar reportes</a></li>";
-        if ($userType === 0) {
+        if ($userType == 1) {
             echo "<li class='sidebar-nav-item'><a href='$rootProjectPath/reports/adminReports.php'>Administrar reportes</a></li>";
         }
         echo "<li class='sidebar-nav-item'><a href='$rootProjectPath/about.php'>Acerca de nosotros</a></li>";
