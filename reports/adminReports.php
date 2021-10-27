434beb5eb43dad3b0a76b3a57b67d2487b11a194
Sergio Cortez <sercor_12@outlook.es>
2021-10-27 08:37:57 -0500

    adjust height of operations column

--- reports/adminReports.php
+++ reports/adminReports.php
@@ -80,57 +80,60 @@ $userType = (isset($_SESSION['tipo_usuario'])) ? $_SESSION['tipo_usuario'] : nul
       ?>
 
       <h4 class="mt-5 mb-3">Reportes</h4>
+      <div class='table-responsive'>
+         <table class='table table-hover'>
+            <thead class='thead-dark'>
+               <tr>
+                  <td class='fw-bold'>ID</td>
+                  <td class='fw-bold'>Estado</td>
+                  <td class='fw-bold'>Usuario</td>
+                  <td class='fw-bold'>Titulo</td>
+                  <td class='fw-bold'>Contenido</td>
+                  <td class='fw-bold'>Imagen</td>
+                  <td class='fw-bold'>Operaciones</td>
+               </tr>
+            </thead>
 
-      <table class='table table-hover'>
-         <thead class='thead-dark'>
-            <tr>
-               <td class='fw-bold'>ID</td>
-               <td class='fw-bold'>Estado</td>
-               <td class='fw-bold'>Usuario</td>
-               <td class='fw-bold'>Titulo</td>
-               <td class='fw-bold'>Contenido</td>
-               <td class='fw-bold'>Imagen</td>
-               <td class='fw-bold'>Operaciones</td>
-            </tr>
-         </thead>
+            <tbody>
+               <?php
+               while ($row = mysqli_fetch_array($liveResults)) {
+                  $title = $row["title"];
+                  $content = $row["content"];
+                  $user = $row["user"];
+                  $id = $row["id"];
+                  $image = $row['image'];
+                  $nResponses = $row['counter_responses'];
+                  $status = ($nResponses == 0) ? "Sin resolver" : "Resuelta";
+                  $statusColor = ($nResponses == 0) ? "warning" : "success";
+                  $statusBgColor = ($nResponses == 0) ? "rgba(255, 193, 7, 0.1)" : "rgba(25, 134, 83, 0.1)";
 
-         <tbody>
-            <?php
-            while ($row = mysqli_fetch_array($liveResults)) {
-               $title = $row["title"];
-               $content = $row["content"];
-               $user = $row["user"];
-               $id = $row["id"];
-               $image = $row['image'];
-               $nResponses = $row['counter_responses'];
-               $status = ($nResponses == 0) ? "Sin resolver" : "Resuelta";
-               $statusColor = ($nResponses == 0) ? "warning" : "success";
-               $statusBgColor = ($nResponses == 0) ? "rgba(255, 193, 7, 0.1)" : "rgba(25, 134, 83, 0.1)";
-
-               printf("<tr ><td>%d</td><td><div style='background-color: $statusBgColor; width: 25px; height: 25px;' class='d-flex  align-items-center justify-content-center'></div></td><td>%s</td><td>%s</td><td>%s</td>
-               <td class='image-container'>", $id, $user, $title, $content,);
-               if ($image != null) {
-                  echo "<i class='show-icon bi bi-image-fill'></i>";
-                  echo "<img class='hidden-image rounded img-fluid' src='../medias/$image'/>";
+                  printf("<tr ><td>%d</td><td><div style='background-color: $statusBgColor; width: 25px; height: 25px;' class='d-flex  align-items-center justify-content-center'></div></td><td>%s</td><td>%s</td><td>%s</td>
+                  <td class='image-container'>", $id, $user, $title, $content,);
+                  if ($image != null) {
+                     echo "<i class='show-icon bi bi-image-fill'></i>";
+                     echo "<img class='hidden-image rounded img-fluid' src='../medias/$image'/>";
+                  }
+                  printf("</td>
+                     <td>
+                     <div class='d-flex align-items-center '>
+                        <a class='text-decoration-none' onclick=\"return confirmSubmit()\" href=\"deleteReport.php?id=%s\">
+                           <i class='bi bi-trash-fill text-danger' style='font-size: 1.25rem;'></i>
+                        </a>
+                        <span style='width: .5rem;'></span>
+                        <a class='text-decoration-none' href=\"updateReport.php?id=%s\">
+                           <i class='bi bi-pencil-fill' style='font-size: 1.25rem;'></i>
+                        </a>
+                     </div>
+                     </td>
+                  </tr>", $id, $id);
                }
-               printf("</td>
-                  <td class='d-flex align-items-center'>
-                     <a class='text-decoration-none' onclick=\"return confirmSubmit()\" href=\"deleteReport.php?id=%s\">
-                        <i class='bi bi-trash-fill text-danger' style='font-size: 1.25rem;'></i>
-                     </a>
-                     <span style='width: .5rem;'></span>
-                     <a class='text-decoration-none' href=\"updateReport.php?id=%s\">
-                        <i class='bi bi-pencil-fill' style='font-size: 1.25rem;'></i>
-                     </a>
-                  </td>
-               </tr>", $id, $id);
-            }
 
-            mysqli_free_result($liveResults);
-            mysqli_close($conn);
-            ?>
-         </tbody>
-      </table>
+               mysqli_free_result($liveResults);
+               mysqli_close($conn);
+               ?>
+            </tbody>
+         </table>
+      </div>
 
       <h4 class="mt-5 mb-3">Reportes eliminados</h4>
 
