05b7a037caf9e27a9f9f910fa39a3b61de37761b
Sergio Cortez <sercor_12@outlook.es>
2021-11-22 09:04:35 -0600

    fix merge garbage

--- conexion.php
+++ conexion.php
@@ -1,32 +1,7 @@
-<<<<<<< HEAD
 <?php
-<<<<<<< HEAD
 
 $conn = new mysqli("localhost", "root", "", "sistemaalumnos");
 
 if ($conn->connect_errno) {
 	echo "No hay conexión: (" . $conn->connect_errno . ") " . $conn->connect_error;
 }
-=======
-	
-<<<<<<< HEAD
-	$conn = new mysqli("localhost","root","root","sistemaalumnos");
-=======
-	$conn = new mysqli("localhost","root","","sistemaalumnos");
->>>>>>> 86d9ac7f980dd6991afba801696a9d4256ba81c4
-	
-	if($conn->connect_errno)
-	{
-		echo "No hay conexión: (" . $conn->connect_errno . ") " . $conn->connect_error;
-		
-	}
-	
-	?>
->>>>>>> rama
-=======
-$conn = new mysqli("localhost", "root", "", "sistemaalumnos");
-
-if ($conn->connect_errno) {
-echo "No hay conexión: (" . $conn->connect_errno . ") " . $conn->connect_error;
-}
->>>>>>> rama
